a51df933ce39791e532ab799fc9d2823225423e8
MagiCarpy <49000672+MagiCarpy@users.noreply.github.com>
2024-10-25 23:05:17 -0700

    Implemented Working computeVotes function (10/25)

--- CS31_HW/CS_Project_3/Project3/poll.cpp
+++ CS31_HW/CS_Project_3/Project3/poll.cpp
@@ -14,6 +14,9 @@ the predicted number of votes for the party D is
 82 (54 from CA plus 28 from NY); for the party r 
 it's 46 (40 from TX plus 6 from MS); for the party 
 L it's 6 (from UT). 
+
+!!!COMPARE WITH FAQ and SPEC!!!
+!!!CHECK ALL THE COMMENTS!!!
 */
 bool hasRightSyntax(string pollData);
 int computeVotes(string pollData, char party, int& voteCount);
@@ -21,10 +24,64 @@ bool isValidUppercaseStateCode(string stateCode);
 bool checkVotes(string pollData, int& index);
 
 int main() {
-    cout << "VALUE: " << hasRightSyntax("R40TXD54CAr6Msd28nYL06UT"); //DEL
+    int votes = 0;
+    //cout << "VALUE: " << hasRightSyntax("R40TXD54CAr6Msd28nYL06UT"); //DEL
+    cout << endl << "VALUE: " << computeVotes("R40TXD54CAr6Msd28nYL06T", 'l', votes) << endl;
+    cout << "VOTES: " << votes;
 }
 
-// returns true if parameter pollData meets definition
+// Modifies voteCount to amount of votes for a specific party from pollData
+int computeVotes(string pollData, char party, int& voteCount) {
+    //used to keep voteCount unmodified for the 3 special cases
+    int totalVotes = 0;
+
+    // checks if pollData is valid sequence
+    if (!hasRightSyntax(pollData)) {
+        return 1;
+    }
+
+    // checks if valid party char
+    if (!isalpha(party)) {
+        return 3;
+    }
+
+    int index = 0;
+    while (index != pollData.size()) {
+        // checks if correct party char for each segment
+        char firstChar = pollData.at(index);//FIXME: Modify this with casting to char?
+        cout << "firstChar:" << firstChar << " currLetter:" << pollData.at(index) << " PCHAR:" << party << "||";
+        cout << "CHAR TEST: " << (static_cast<char>(tolower(party)) == static_cast<char>(tolower(firstChar))); //DEL
+        if (static_cast<char>(tolower(party)) == static_cast<char>(tolower(firstChar))) { //FIXME: ADD THIS CASTING SYNTAX EVERYWHERE???
+            cout << "PASSED: ";
+            index++;
+            string numVotes;
+            for (numVotes = ""; isdigit(pollData.at(index)); index++) {
+                numVotes += pollData.at(index);
+            }
+            // check if any state has 0 votes
+            if (stoi(numVotes) == 0) {
+                return 2;
+            }
+            // add to temporary total vote counter
+            totalVotes += stoi(numVotes);
+
+            // index past digit -> goto next party index
+            index += 2;
+        } else {
+            // goto to the next segment
+            index++;
+            while (isdigit(pollData.at(index))) {
+                index++;
+            }
+            index += 2;
+        }
+    }
+    // modify referenced voteCount count is successful
+    voteCount = totalVotes;
+    return 0;
+}
+
+// Checks whether parameter pollData meets definition/syntax
 bool hasRightSyntax(string pollData) {
     if (pollData == "") {
         return true;
@@ -35,7 +92,7 @@ bool hasRightSyntax(string pollData) {
     while (index != pollData.size()) {
         cout << "*" << pollData << "*"; //DEL
         cout << " LOOP:" << index << " "; //DEL
-        //check if each poll sequence is valid (party, votes, state)
+        // check if each poll sequence is valid (party, votes, state)
         if (!checkVotes(pollData, index)) {
             return false;
         }
@@ -43,25 +100,9 @@ bool hasRightSyntax(string pollData) {
     return true;
 }
 
-int computeVotes(string pollData, char party, int& voteCount) {
-    return 1;
-}
-
-// Return true if the argument is a two-uppercase-letter state code, or
-// false otherwise.
-bool isValidUppercaseStateCode(string stateCode)
-{
-    const string codes =
-        "AL.AK.AZ.AR.CA.CO.CT.DE.DC.FL.GA.HI.ID.IL.IN.IA.KS."
-        "KY.LA.ME.MD.MA.MI.MN.MS.MO.MT.NE.NV.NJ.NH.NM.NY.NC."
-        "ND.OH.OK.OR.PA.RI.SC.SD.TN.TX.UT.VT.VA.WA.WV.WI.WY";
-    return (stateCode.size() == 2 &&
-        stateCode.find('.') == string::npos &&  // no '.' in stateCode
-        codes.find(stateCode) != string::npos);  // match found
-}
-
+// Checks whether each segment of pollData meets definition/syntax
 bool checkVotes(string pollData, int& index) {
-    char firstChar = pollData.at(index);
+    char firstChar = pollData.at(index); //CHECK GLOBAL VARIABLE?
     if (isalpha(firstChar)) {
         index++;
     } else {
@@ -70,7 +111,7 @@ bool checkVotes(string pollData, int& index) {
     cout << "vote:" << index; //DEL
     // index past the party -> check number of votes
     string numVotes;
-    // FIXME: Ask if second conditional is required for no vote input (EX. D0CA -> DCA)
+    // DEL: test no vote input (EX. D0CA -> DCA)
     if (index == pollData.size() || !isdigit(pollData.at(index))) {
         return false;
     }
@@ -87,6 +128,19 @@ bool checkVotes(string pollData, int& index) {
         stateCode += toupper(pollData.at(index));
     }
     cout << " final:" << index << endl; //DEL
-    // index ready  to next check next poll sequence
+    // index ready to next check next poll sequence
     return isValidUppercaseStateCode(stateCode);
+}
+
+// Return true if the argument is a two-uppercase-letter state code, or
+// false otherwise.
+bool isValidUppercaseStateCode(string stateCode)
+{
+    const string codes =
+        "AL.AK.AZ.AR.CA.CO.CT.DE.DC.FL.GA.HI.ID.IL.IN.IA.KS."
+        "KY.LA.ME.MD.MA.MI.MN.MS.MO.MT.NE.NV.NJ.NH.NM.NY.NC."
+        "ND.OH.OK.OR.PA.RI.SC.SD.TN.TX.UT.VT.VA.WA.WV.WI.WY";
+    return (stateCode.size() == 2 &&
+        stateCode.find('.') == string::npos &&  // no '.' in stateCode
+        codes.find(stateCode) != string::npos);  // match found
 }
