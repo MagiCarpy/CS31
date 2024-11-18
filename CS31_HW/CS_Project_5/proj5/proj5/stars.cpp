4f72545393b647cf185dd5ff4887ac23b4a19fbb
MagiCarpy <49000672+MagiCarpy@users.noreply.github.com>
2024-11-17 18:44:18 -0800

    final maybe check with spec later

--- CS31_HW/CS_Project_5/proj5/proj5/stars.cpp
+++ CS31_HW/CS_Project_5/proj5/proj5/stars.cpp
@@ -5,7 +5,7 @@
 #include <iostream>
 using namespace std;
 
-const char WORDFILENAME[] = "./myWords.txt"; //DEL: Look at spec
+const char WORDFILENAME[] = "./myWords.txt";
 const int MAXWORDS = 8000;
 
 int runOneRound(const char words[][MAXWORDLEN + 1], int nWords, int wordnum);
@@ -17,51 +17,26 @@ int main()
 {
     /*
     REREAD SPEC
-    
-    1. The getWords function must return an int no greater than maxWords.
-    If it returns a value less than 1, your main routine must write
-    No words were loaded, so I can't play the game.
 
-    2. Add randomize secret
-
-    3. You must call getWords exactly once, before you start playing 
-    any of the rounds of the game.
-
-    4. The getWords function must return an int no greater than maxWords. 
-    If it returns a value less than 1, your main routine must write:
-        No words were loaded, so I can't play the game.
-
-    5. Your program must call this function to play each round of the 
-    game. Notice that this function does not select a random number 
-    and does not tell the user the length of the secret word; the 
-    caller of this function does, and passes the random number as 
-    the third argument. Notice also that this function does not write 
-    the message about the player successfully determining the secret word;
    */
 
-    // DEL: main routine
-
+    // main routine
+ 
     char wordlist[MAXWORDS][MAXWORDLEN + 1];
     int numWordsLoaded = getWords(wordlist, MAXWORDS, WORDFILENAME);
-
-    //DEL: DELETE This code
-    /*
-    for (int i = 0; i < numWordsLoaded; i++) {
-        cout << wordlist[i] << " ";
-    }
-    cout << endl << numWordsLoaded << endl;
-    */
+    int totalRounds = 0;
 
     if (numWordsLoaded < 1) {
         cout << "No words were loaded, so I can't play the game.";
     }
-    // prompt and check for number of rounds
-    cout << "How many rounds do you want to play? ";
-    int totalRounds;
-    cin >> totalRounds;
-    cin.ignore(1000, '\n');
-    if (totalRounds <= 0) {
-        cout << "The number of rounds must be positive.";
+    else {
+        // prompt and check for number of rounds
+        cout << "How many rounds do you want to play? ";
+        cin >> totalRounds;
+        cin.ignore(1000, '\n');
+        if (totalRounds <= 0) {
+            cout << "The number of rounds must be positive.";
+        }
     }
 
     // score stats
@@ -74,7 +49,7 @@ int main()
     while (roundNum < totalRounds) {
         roundNum++;
         int secretRandInt; // random int/index for secret
-        secretRandInt = randInt(0, numWordsLoaded-1); // FIXME: figure out numWords
+        secretRandInt = randInt(0, numWordsLoaded-1);
 
         cout << endl << "Round " << roundNum << endl;
         cout << "The secret word is " << strlen(wordlist[secretRandInt]) << " letters long." << endl;
@@ -103,47 +78,40 @@ int main()
         double average = totalScore / static_cast<double>(roundNum);
         cout.setf(ios::fixed);
         cout.precision(2);
-        cout << "Average: " << average << ", minimum : " << minScore
-             << ", maximum : " << maxScore << endl; 
+        cout << "Average: " << average << ", minimum: " << minScore
+             << ", maximum: " << maxScore << endl; 
 
-        /* DEL: The average number of trials must be written with a decimal
-           point and exactly two digits to the right of the decimal point. 
-        */
     }
-
-        
 
 }
 
 int runOneRound(const char words[][MAXWORDLEN+1], int nWords, int wordnum) {
-    if (nWords < 0 || wordnum >= nWords) {
+    if (nWords <= 0 || wordnum < 0 || wordnum >= nWords) {
         return -1;
     }
     char trialInp[100];
     int score = 0;
     for (;;) {
-        /*
-        DEL: Notice that unknown words and trial strings that don't consist of 
-        exactly 4 to 6 lower case letters don't count toward the number 
-        of tries for a round. The average number of trials must be 
-        written with a decimal point and exactly two digits 
-        to the right of the decimal point.
-        */
+        
         cout << "Trial word: ";
         cin.getline(trialInp, 100);
+
+        // check trial word length
         if (strlen(trialInp) > MAXWORDLEN || strlen(trialInp) < MINWORDLEN || !stringIsLower(trialInp)) {
             cout << "Your trial word must be a word of 4 to 6 lower case letters." << endl;
             continue;
         }
+        // check if trial word in wordlist
         if (!inValidWords(words, nWords, trialInp)) {
             cout << "I don't know that word." << endl;
             continue;
         }
+        // check if trail word is equal to secret
         if (strcmp(words[wordnum], trialInp) == 0) {
             score++;
             break;
         }
-
+        // otherwise calculate planets and stars
         calcPlanetStars(trialInp, words[wordnum]);
         score++;
     }
@@ -153,7 +121,7 @@ int runOneRound(const char words[][MAXWORDLEN+1], int nWords, int wordnum) {
 
 void calcPlanetStars(const char trialWord[], const char secret[]) {
     // copy made to keep track of matched planets and stars
-    char secretCopy[MAXWORDLEN+1]; // FIXME: change length == secret max length?
+    char secretCopy[MAXWORDLEN+1];
     strcpy(secretCopy, secret);
     char trialCopy[MAXWORDLEN+1];
     strcpy(trialCopy, trialWord);
@@ -170,7 +138,6 @@ void calcPlanetStars(const char trialWord[], const char secret[]) {
     }
     // match stars first
     for (int i = 0; i < maxInd; i++) {
-        // FIXME: check diff secret/trial len (ex. secret: egret trial: egrett ask)
         if (trialWord[i] == secretCopy[i]) {
             stars++;
             // period signifies already matched star
@@ -196,6 +163,8 @@ void calcPlanetStars(const char trialWord[], const char secret[]) {
     cout << "Stars: "<< stars << ", Planets: " << planets << endl;
 }
 
+
+// Check if trail word is in the wordlist
 bool inValidWords(const char words[][MAXWORDLEN+1], int nWords, const char cstring[]) {
     for (int i = 0; i < nWords; i++) {
         if (strcmp(words[i],cstring) == 0) {
@@ -205,6 +174,7 @@ bool inValidWords(const char words[][MAXWORDLEN+1], int nWords, const char cstri
     return false;
 }
 
+// Check if C string is all lowercase
 bool stringIsLower(const char cstring[]) {
     for (int i = 0; cstring[i] != '\0'; i++) {
         if (!islower(cstring[i])) {
